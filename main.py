16659e7093d5afa57dcc7da5fdfe5f8271cd472a
Naveen Dharmathunga <dnd.pro@outlook.com>
2024-07-30 12:53:01 +0530

    Add main script and header

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+import os
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# download zip file from https://fontawesome.com/ and extract into fontawesome directory.
+INPUT_FILE = os.path.join("fontawesome", "metadata", "icons.json")
+OUTPUT_FILE = 'fontawesome6.sty'
+
+OUTPUT_HEADER = r'''
+% Identify this package.
+\NeedsTeXFormat{LaTeX2e}
+\ProvidesPackage{fontawesome6}[2023/01/07 v5.0.2 font awesome icons]
+% Requirements to use.
+\usepackage{fontspec}
+% Configure a directory location for fonts(default: 'fonts/')
+\newcommand*{\fontdir}[1][fonts/]{\def\@fontdir{#1}}
+\fontdir
+% Define pro option
+\DeclareOption{pro}{
+  % Define shortcut to load the Font Awesome pro font.
+  \newfontfamily\FA[
+    Path=\@fontdir,
+    UprightFont=*-Regular-400,
+    ItalicFont=*-Light-300,
+    BoldFont=*-Solid-900,
+  ]{Font Awesome 6 Pro}
+}
+\ProcessOptions\relax
+% Define shortcut to load the Font Awesome font for brands.
+\newfontfamily{\FAbrands}[Path=\@fontdir]{Font Awesome 6 Brands-Regular-400}
+% Define shortcut to load the Font Awesome font.
+\@ifundefined{FA}{%
+\newfontfamily\FA[
+  Path=\@fontdir,
+  UprightFont=*-Regular-400,
+  BoldFont=*-Solid-900,
+]{Font Awesome 6 Free}
+}{}
+% Generic command displaying an icon by its name.
+\newcommand*{\faicon}[1]{{
+  \csname faicon@#1\endcsname
+}}
+'''
+
+OUTPUT_LINE = '\expandafter\def\csname faicon@%(name)s\endcsname {%(font)s\symbol{"%(symbol)s}} \n'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    with open(INPUT_FILE, 'r') as json_data:
+        icons = json.load(json_data)
+        with open(OUTPUT_FILE, 'w') as w:
+            w.write(OUTPUT_HEADER)
+            for icon_name in sorted(icons.keys()):
+                font = "\FA" if "brands" not in icons[icon_name]["styles"] else "\FAbrands"
+                w.write(
+                    OUTPUT_LINE % {
+                        'name': icon_name, 'symbol': icons[icon_name]["unicode"].upper(), "font": font
+                    }
+                )
+            w.write(r'\endinput')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
